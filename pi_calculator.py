09ed003a38d2212dd512046dc775aae78cdb0d69
abhishekchaturvedi1995 <ashuchaturvedi195@gmail.com>
2020-02-26 10:53:46 +0530

    Add files via upload

--- pi_calculator.py
+++ pi_calculator.py
@@ -1,22 +1,11 @@
 from random import randint, random, uniform
-
+import pandas as pd
 incircle=0
 outcircle=0
 for i in range(100000):	
 	x=uniform(1,10)
 	y=uniform(1,10)
-	print tuple((x,y))
-	if(((x-5.5)**2+(y-5.5)**2)<=(4.5*4.5)):
-		incircle += 1
-	else:
-		outcircle += 1
-	totalpoints = incircle + outcircle
-	pi = 4*float(float(incircle)/float(totalpoints))
-	print "In circle points - "+str(incircle)
-	print "outcircle points - "+str(outcircle)
-	print "total points - "+str(totalpoints)
-	print "PIE"
-	print pi
+	
 
 
 
